ba0fa44582b2c9e49d6c7ed58f1787edd90ff5e0
Jeong-soo, Kim <jsootv@gmail.com>
2024-02-18 01:38:17 +0900

    Add files via upload

---
+++ download/MemberRole.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNet.ApiService.Data.DomainModel;
+/// <summary>
+/// 회원 권한
+/// </summary>
+public class MemberRole
+{
+    /// <summary>
+    /// 권한 유형
+    /// </summary>
+    [Key, StringLength(50), Column(TypeName = "varchar(50)")]
+    public string RoleType { get; set; } = default!;
+
+    /// <summary>
+    /// 권한 아이디
+    /// </summary>
+    [Key, StringLength(50), Column(TypeName = "varchar(50)")]
+    public string RoleId { get; set; } = default!;
+
+    /// <summary>
+    /// 권한명
+    /// </summary>
+    [Required, StringLength(100), Column(TypeName = "nvarchar(100)")]
+    public string RoleName { get; set; } = default!;
+
+    /// <summary>
+    /// 권한설명
+    /// </summary>
+    [Required, StringLength(100), Column(TypeName = "nvarchar(100)")]
+    public string RoleDesc { get; set; } = default!;
+
+    /// <summary>
+    /// 권한 우선순위
+    /// </summary>
+    [Required]
+    public byte RolePriority { get; set; }
+
+    /// <summary>
+    /// 등록일시
+    /// </summary>
+    [Required]
+    [DataType(DataType.DateTime)]
+    public System.DateTime CreatedDate { get; set; }
+
+    /// <summary>
+    /// 수정일시
+    /// </summary>
+    [Required]
+    [DataType(DataType.DateTime)]
+    public System.DateTime ModifiedDate { get; set; }
+
+    /// <summary>
+    /// 회원보유권한 리스트
+    /// </summary>
+    public ICollection<MemberRolesByMember> MemberRolesByMembers { get; set; } = default!;
+}
---
+++ download/MemberRolesByMember.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNet.ApiService.Data.DomainModel;
+/// <summary>
+/// 회원별 보유권한
+/// </summary>
+public class MemberRolesByMember
+{
+    /// <summary>
+    /// 회원아이디
+    /// </summary>
+    [Key, StringLength(50), Column(TypeName = "varchar(50)")]
+    public string MemberId { get; set; } = default!;
+
+    /// <summary>
+    /// 권한유형(website)
+    /// </summary>
+    [Key, StringLength(50), Column(TypeName = "varchar(50)")]
+    public string RoleType { get; set; } = default!;
+
+    /// <summary>
+    /// 권한아이디
+    /// </summary>
+    [Key, StringLength(50), Column(TypeName = "varchar(50)")]
+    public string RoleId { get; set; } = default!;
+
+    /// <summary>
+    /// 수여자 아이디
+    /// </summary>
+    [Required, StringLength(50), Column(TypeName = "varchar(50)")]
+    public string GrantorId { get; set; } = default!;
+
+    /// <summary>
+    /// 권한소유일시
+    /// </summary>
+    [Required]
+    [DataType(DataType.DateTime)]
+    public System.DateTime OwnedDate { get; set; }
+
+    /// <summary>
+    /// 회원 마스터
+    /// </summary>
+    public Member Member { get; set; } = default!;
+
+    /// <summary>
+    /// 회원 권한
+    /// </summary>
+    public MemberRole MemberRole { get; set; } = default!;
+}
